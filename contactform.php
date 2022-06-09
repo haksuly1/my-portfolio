ed1cad4314645e6d6341b431528ac04c73431628
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-06-09 22:10:33 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -2,12 +2,13 @@
 <?php
 
 //Get data for online contact form
-/*
-if (isset($_POST['submit'])) {
+if(isset($_POST['submit'])) {
   $name = $_POST['name'];
+  $telephone = $_POST['telephone'];
   $subject = $_POST['subject'];
-  $mailFrom = $_POST['email'];
   $message = $_POST['message'];
+  $mailFrom = $_POST['email'];
+  
 
 
   $mailTo = "hakeemkayodesulyman@gmail.com";
@@ -15,24 +16,8 @@ if (isset($_POST['submit'])) {
   $txt = "You have received an email from ".$name.".\r\n".$message;
 
   mail($mailFrom, $subject, $txt, $headers);
-  header("Location: index.php?mailsend");
+  header("Location: thankyou.html");
+  //header("Location: index.php?mailsend");
 }
-*/
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$telephone = $_POST['telephone'];
-$message = $_POST['message'];
-$subject = $_POST['subject'];
-
-$to = "hakeemkayodesulyman@gmail.com";
-$subject = "Your have received an email from your online contact form";
-$text ="Name = ". $name . "\r\n Email = " . $email . "\r\nTelephone = " .$telephone . "\r\nMessage = " . $message . "\r\nSubject = " . $subject;
-$headers = "From: $email";
-if($email!=NULL){
-  mail($to,$subject.$txt,$headers);
-}
-
-header("Location:thankyou.html");
 
 ?>
