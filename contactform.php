02d7062bac0b49fabb20d9639c2edbfb2228c4c4
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-06-09 22:18:45 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -13,6 +13,27 @@ if(isset($_POST['submit'])) {
 
   $mailTo = "hakeemkayodesulyman@gmail.com";
   $headers = "From: ".$mailFrom;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($mailFrom, $subject, $txt, $headers);
+  header("Location: thankyou.html");
+  //header("Location: index.php?mailsend");
+}
+
+
+/*
+//Get data for online contact form
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $telephone = $_POST['telephone'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $mailFrom = $_POST['email'];
+  
+
+
+  $mailTo = "hakeemkayodesulyman@gmail.com";
+  $headers = "From: ".$mailFrom;
   $txt = "You have received an email from ".$name.".\r\n".$message;
 
   mail($mailFrom, $subject, $txt, $headers);
@@ -20,4 +41,5 @@ if(isset($_POST['submit'])) {
   //header("Location: index.php?mailsend");
 }
 
+*/
 ?>
