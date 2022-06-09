1da152cf9fbbfe1a4a8f2688cd9d40732dce7361
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-06-09 11:30:32 +0200

    Update contactform.php
    edit for contact form transfer

--- contactform.php
+++ contactform.php
@@ -1,9 +1,12 @@
+
 <?php
 
+//Get data for online contact form
+/*
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $subject = $_POST['subject'];
-  $mailFrom = $_POST['mail'];
+  $mailFrom = $_POST['email'];
   $message = $_POST['message'];
 
 
@@ -14,3 +17,21 @@ if (isset($_POST['submit'])) {
   mail($mailFrom, $subject, $txt, $headers);
   header("Location: index.php?mailsend");
 }
+*/
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+
+$to = "hakeemkayodesulyman@gmail.com";
+$subject = "Your have received an email from your online contact form";
+$text ="Name = ". $name . "\r\n Email = " . $email . "\r\nTelephone = " .$telephone . "\r\nMessage = " . $message . "\r\nSubject = " . $subject;
+$headers = "From: $email" . "\r\n" <div class="if(email=NULL){
+  mail($to,$subject.$txt,$headers);
+}
+
+header("Location:thankyou.html");
+
+?>
